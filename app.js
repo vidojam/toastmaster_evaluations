edcf0cb76d7be781c1e5e1fd0296a4c39067e38b
Jose Torres <vidojam@gmail.com>
2024-11-06 17:21:46 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,118 @@
+function showAlertOpening() {
+    alert("Fellow Toastmasters, guest and especially...[speaker name]");
+    alert("That was an outstanding speech");
+    alert("The structure of your speech, is the basis, for all good presentations");
+    alert("A strong introduction, a clear body and a powerful closing");
+}
+
+function showAlertIntro() {
+    alert("In your introduction...");
+    alert("You made us think, grabbed our attention and told us, what your topic was about")
+    alert("You ask a question, that made us think");
+    alert("You made a statement, that grabbed our attention");
+    alert("You set the stage for your speech");
+}  
+
+function showAlertBody() {
+     alert("In the body...");
+     alert("You had the perfect combination, to deliver your message");
+     alert("It was clear, concise and entertaining");
+}  
+
+        function showAlertStage() {
+            alert("My compliments on the use of your stage area.");
+            alert("I suggest adding a little more variety");
+            alert("Downstage communicates more intimacy and connection with the audience");
+            alert("Or you can use different parts for different parts of your speech");
+        }  
+
+        function showAlertEye() {
+            alert("With your eye contact...");
+            alert("You bonded with the audience...");
+            alert("You communicated passion and strengthen your message");
+        }  
+
+        function showAlertGestures() {
+            alert("Your gestures were natural and appropriate");
+            alert("When well timed, gestures can add impact to your message");
+            alert("They emphasized and reinforced your message");
+           
+        }  
+
+
+        function showAlertStories() {
+            alert("Who does not love a good story?");
+            alert("You engaged and hooked your audience with your stories");
+            alert("You also created an emotional and memorable connection, with the audience");
+        }  
+
+
+        function showAlertProps() {
+            alert("Props...");
+            alert("Make your speech MORE INTERESTING to listen to");
+            alert("Allows the audience to VISUALIZE your message.");
+        }  
+
+
+        function showAlertVocalVariety() {
+            alert("Your Vocal Variety, breaths life into your words and actions");
+            alert("It also makes you more interesting to listen to");
+        }  
+
+                function showAlertVolume() {
+                    alert("Volume, to ensure your audience can hear you and emphasize important points");
+                    alert("find the right volume balance, to keep your audience engaged");
+                }  
+                function showAlertPitch() {
+                    alert("Pitch, higher pitch can communicate excitement, to emphasize your key points");
+                    alert("Lower pitch can communicate seriousness, to emphasize your key points"); 
+                }  
+
+                function showAlertEmphasis() {
+                    alert("Emphasis, stress cert words, to highlight your key points");
+                    alert("Repeating key words or phrases, to emphasize your key points");
+                }  
+                function showAlertPause() {
+                    alert("Pause, to punctuate your message, and to create impact and let your audience reflect");
+                    alert("Before and after words or key points, to let your audience reflect");
+                }  
+                function showAlertPace() {
+                    alert("Pace, to keep your audience engaged");
+                    alert("On Pace, keep it slow to generate suspense and fast to generate excitement");
+                    alert("conversation is at a rate of 120-150 words per minute");
+                }  
+                function showAlertEmotion() {
+                    alert("Emotion, i.e. a smile with a happy memory, to connect with your audience");
+                }  
+
+
+
+function showAlertClose() {
+            alert("In your close...");
+            alert("You reinforced your message and left a lasting impression");
+            alert("Came full circle and ended with a powerful closing");
+            alert("Gave us a call to action, to reflect on your message");
+}  
+
+function showAlertSuggestion() {
+    alert("My suggestion, to take this speech to the next level...");
+    
+    alert("Is to use more Vocal Variety");
+    alert("Vocal Variety, breaths life into your words and actions");
+    alert("Vocal Variety, creates an emotional and memorable connection with your audience");
+
+    alert("Volume, to ensure your audience can hear you and emphasize important points");
+    alert("Pitch, higher pitch can communicate excitement, to emphasize your key points");
+    alert("Emphasis, stress certain words, to highlight your key points");
+    alert("Pace, to keep your audience engaged");
+    alert("Pause, to punctuate your message, and to create impact and let your audience reflect");
+    alert("Emotion, smile with a happy memory, to connect with your audience"); 
+
+    alert("It will help you to create more impact and keep your audience engaged");
+}  
+
+function showAlertSummary() {
+     alert("In summary...");
+     alert("Keep up the x, y and z and add vocal variety to your speech");
+     alert("I am definitely looking forward, to your next speech");
+}    
