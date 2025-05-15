b9b4d32005101b6e41320f32a101b7c50c85bdd9
Jose Torres <Vidojam@gmail.com>
2025-05-14 21:39:43 -0400

    updated good deliveries and sugestions

--- app.js
+++ app.js
@@ -1,118 +1,121 @@
 function showAlertOpening() {
     alert("Fellow Toastmasters, guest and especially...[speaker name]");
     alert("That was an outstanding speech");
-    alert("The structure of your speech, is the basis, for all good presentations");
     alert("A strong introduction, a clear body and a powerful closing");
+    alert("Is the basis, for all good speech presentations");
 }
 
 function showAlertIntro() {
     alert("In your introduction...");
-    alert("You made us think, grabbed our attention and told us, what your topic was about")
-    alert("You ask a question, that made us think");
+    alert("You made us think, grabbed our attention and told us, what your topic was about or...");
+    alert("You asked a question, that made us think and set the stage for your speech");
     alert("You made a statement, that grabbed our attention");
-    alert("You set the stage for your speech");
-}  
+}
 
 function showAlertBody() {
-     alert("In the body...");
-     alert("You had the perfect combination, to deliver your message");
-     alert("It was clear, concise and entertaining");
-}  
+    alert("Your body had the perfect combination, to deliver your message");
+    alert("Your speech was clear, concise and entertaining");
+}
 
-        function showAlertStage() {
-            alert("My compliments on the use of your stage area.");
-            alert("I suggest adding a little more variety");
-            alert("Downstage communicates more intimacy and connection with the audience");
-            alert("Or you can use different parts for different parts of your speech");
-        }  
+function showAlertStage() {
+    alert("My compliments on the use of your stage area.");
+    alert("Downstage you communicated more intimacy and connection with the audience");
+    alert("You used different parts for different parts of your speech");
+}
 
-        function showAlertEye() {
-            alert("With your eye contact...");
-            alert("You bonded with the audience...");
-            alert("You communicated passion and strengthen your message");
-        }  
+function showAlertEye() {
+    alert("With your eye contact...");
+    alert("You bonded with the audience...");
+    alert("You communicated passion and strengthened your message");
+}
 
-        function showAlertGestures() {
-            alert("Your gestures were natural and appropriate");
-            alert("When well timed, gestures can add impact to your message");
-            alert("They emphasized and reinforced your message");
-           
-        }  
+function showAlertGestures() {
+    alert("Your gestures were natural and appropriate");
+    alert("Well-timed, the gestures added impact to your message");
+    alert("They also emphasized and reinforced your message");
+}
 
+function showAlertStories() {
+    alert("Who does not love a good story?");
+    alert("You engaged and hooked your audience with your stories");
+    alert("You also created an emotional and memorable connection with the audience");
+}
 
-        function showAlertStories() {
-            alert("Who does not love a good story?");
-            alert("You engaged and hooked your audience with your stories");
-            alert("You also created an emotional and memorable connection, with the audience");
-        }  
+function showAlertProps() {
+    alert("Props...");
+    alert("Make your speech MORE INTERESTING to listen to");
+    alert("Allow the audience to VISUALIZE your message.");
+}
 
-
-        function showAlertProps() {
-            alert("Props...");
-            alert("Make your speech MORE INTERESTING to listen to");
-            alert("Allows the audience to VISUALIZE your message.");
-        }  
-
-
-        function showAlertVocalVariety() {
-            alert("Your Vocal Variety, breaths life into your words and actions");
-            alert("It also makes you more interesting to listen to");
-        }  
-
-                function showAlertVolume() {
-                    alert("Volume, to ensure your audience can hear you and emphasize important points");
-                    alert("find the right volume balance, to keep your audience engaged");
-                }  
-                function showAlertPitch() {
-                    alert("Pitch, higher pitch can communicate excitement, to emphasize your key points");
-                    alert("Lower pitch can communicate seriousness, to emphasize your key points"); 
-                }  
-
-                function showAlertEmphasis() {
-                    alert("Emphasis, stress cert words, to highlight your key points");
-                    alert("Repeating key words or phrases, to emphasize your key points");
-                }  
-                function showAlertPause() {
-                    alert("Pause, to punctuate your message, and to create impact and let your audience reflect");
-                    alert("Before and after words or key points, to let your audience reflect");
-                }  
-                function showAlertPace() {
-                    alert("Pace, to keep your audience engaged");
-                    alert("On Pace, keep it slow to generate suspense and fast to generate excitement");
-                    alert("conversation is at a rate of 120-150 words per minute");
-                }  
-                function showAlertEmotion() {
-                    alert("Emotion, i.e. a smile with a happy memory, to connect with your audience");
-                }  
-
-
-
-function showAlertClose() {
-            alert("In your close...");
-            alert("You reinforced your message and left a lasting impression");
-            alert("Came full circle and ended with a powerful closing");
-            alert("Gave us a call to action, to reflect on your message");
-}  
-
-function showAlertSuggestion() {
-    alert("My suggestion, to take this speech to the next level...");
-    
-    alert("Is to use more Vocal Variety");
-    alert("Vocal Variety, breaths life into your words and actions");
-    alert("Vocal Variety, creates an emotional and memorable connection with your audience");
-
+function showAlertVocalVariety() {
+    alert("Your Vocal Variety breathes life into your words and actions");
+    alert("It also makes you more interesting to listen to");
     alert("Volume, to ensure your audience can hear you and emphasize important points");
+    alert("Find the right volume balance, to keep your audience engaged");
     alert("Pitch, higher pitch can communicate excitement, to emphasize your key points");
     alert("Emphasis, stress certain words, to highlight your key points");
     alert("Pace, to keep your audience engaged");
     alert("Pause, to punctuate your message, and to create impact and let your audience reflect");
-    alert("Emotion, smile with a happy memory, to connect with your audience"); 
+    alert("Emotion, smile with a happy memory, to connect with your audience");
+}
 
-    alert("It will help you to create more impact and keep your audience engaged");
-}  
+function showAlertClose() {
+    alert("In your closing...");
+    alert("You ended with a powerful closing");
+    alert("You left us with a strong message, to reflect on");
+    alert("Came full circle and ended with a powerful closing");
+    alert("Gave us a call to action, to reflect on your message");
+    alert("In your close, you reinforced your message and left a lasting impression");
+}
 
-function showAlertSummary() {
-     alert("In summary...");
-     alert("Keep up the x, y and z and add vocal variety to your speech");
-     alert("I am definitely looking forward, to your next speech");
-}    
+function showAlertSuggestions() {
+    alert("My suggestion, to take this speech to the next level...");
+}
+
+function showAlertStageSug() {
+    alert("Try to make more use of your stage area.");
+    alert("Downstage communicates more intimacy and connection with the audience");
+    alert("Or you can use different parts for different parts of your speech");
+}
+
+function showAlertEyeContactSug() {
+    alert("Use more eye contact and bond with the audience...");
+    alert("This way you communicate passion and strengthen your message");
+}
+
+function showAlertGesturesSug() {
+    alert("Use more gestures to add impact to your message");
+    alert("They will emphasize and reinforce your message");
+}
+
+function showAlertStoriesSug() {
+    alert("You can engage and hook your audience with your stories");
+    alert("You can also create an emotional and memorable connection with the audience");
+}
+
+function showAlertPropsSug() {
+    alert("Use props to make your speech MORE INTERESTING to listen to");
+    alert("Use props so your audience can VISUALIZE your message.");
+}
+
+function showAlertVocalVarietySug() {
+    alert("Use more vocal variety to take your speech to the next level...");
+    alert("Volume, to ensure your audience can hear you and emphasize important points");
+    alert("Pitch, higher pitch can communicate excitement, lower pitch for seriousness");
+    alert("Emphasis, stress certain words to highlight key points");
+    alert("Pace, to keep your audience engaged");
+    alert("Pause, to punctuate your message and create impact");
+    alert("Emotion, smile with a happy memory, to connect with your audience");
+}
+
+function showAlertKeepUp() {
+    alert("Keep up the good work with eye contact, gestures and stories... ");
+}
+
+function showAlertInclude() {
+    alert("and include stage movement or props or vocal variety to take your speech to the next level");
+}
+
+function showAlertLookingForward() {
+    alert("I am definitely looking forward to your next speech");
+}
